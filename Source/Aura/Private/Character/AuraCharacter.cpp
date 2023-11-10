8eca135ff84f2eb3cfe6bc7679584c9f83ec4c5d
LSH <dltpgns97@gmail.com>
2023-11-10 15:10:44 +0900

    게임모드 세팅 및 캐릭터 움직임 설정 C++ 변경사항 GameModeBase를 상속받는 AuraGameModeBase클래스 생성 AuraCharacter 에서 평면 움직임을 위한 세팅 추가 ---------------------------------------------------------------------------------------- UNREAL 변경사항 AuraCharacter 블루프인트에 스프링암 및 카메라 추가 스프링암 및 카메라 폰 제어 활용 false 설정 스프링암 상속 false 설정 Aura 애니메이션 플루프린트 Idle, Running 분리 캐릭터의 속도가 를 기준으로 애니메이션이 변경되도록 이벤트 그래프 수정

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -2,4 +2,19 @@
 
 
 #include "Character/AuraCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+AAuraCharacter::AAuraCharacter()
+{
+	// 캐릭터의 이동방향으로 캐릭터를 홰전, 초당 400도의 회전속도
+	// 캐릭터의 움직임을 평면으로 제한, 시작시 캐릭터가 평면을 벗어나있으면 평면에서 시작
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 800.f, 0.f);
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	// 카메라 회전시 캐릭터가 움직이지 않음
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
