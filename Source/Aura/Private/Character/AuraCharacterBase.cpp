71e79c0e2da02348ee12ec5353a596494548dcb8
LSH <dltpgns97@gmail.com>
2023-12-04 17:37:37 +0900

    제자리 공격 및 발사체 방향 지정 C++ 변경사항 AuraCharacterBase에서 캐릭터와 카메라 충돌 무시 추가 AuraProjectileSpell에서 SpawnProjectile시 클릭 위치를 받아 발사체 방향 계산 AuraPlayerController에서 shift키에 대한 바인딩 추가 ---------------------------------------------------------------------------------------- UNREAL 변경사항 shift키 인풋 액션 추가 인풋 매핑 컨텍스트에 shift 인풋 액션 추가 GA_FireBolt 블루프린트에서 SpawnProjectile 호출시 클릭위치 전달

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -1,11 +1,17 @@
+
 #include "Character/AuraCharacterBase.h"
+ 
 #include "AbilitySystemComponent.h"
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "Components/CapsuleComponent.h"
 
 AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+	GetMesh()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+	
 	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon"); // Weapon 오브젝트 생성
 	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket")); // 오브젝츠 부착
 	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision); // 충돌 제거
